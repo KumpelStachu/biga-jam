976f702696cae92c739083246ecc8f4d264cfdd2
KumpelStachu <stanislaw.rzeszut@gmail.com>
2023-08-31 01:37:28 +0200

    game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,31 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameManager : MonoBehaviour {
+    [SerializeField] private GameObject[] roomPrefabs;
+
+    void Start() {
+        GenerateRooms(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+    }
+
+    private void GenerateRooms(int level) {
+        var size = (int)Math.Pow(level + 1, 1.1);
+
+        for (int i = -size; i < size + 1; i++) {
+            for (int j = 0; j < size * 2; j++) {
+                if (i == 0 && j == 0) continue;
+                Instantiate(GetRandomRoom(), new Vector2(i * 160f / 9, j * 10), Quaternion.identity);
+            }
+        }
+    }
+
+    private GameObject GetRandomRoom() {
+        return roomPrefabs.ElementAt(UnityEngine.Random.Range(0, roomPrefabs.Length));
     }
 }
