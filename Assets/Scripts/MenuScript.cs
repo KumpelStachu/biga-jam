cb5dc24c555502e08e9fa482fb3c73cb4d249be6
Szymonq1 <126352791+Szymonq1@users.noreply.github.com>
2023-08-31 01:36:39 +0200

    Add Menu Animations

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
+
+    [SerializeField] private Animator menu_animator;
     public void ButtonStart() {
-        SceneManager.LoadScene("SampleScene");
+        menu_animator.Play("Main_menu");
+        Invoke("Start_game", 1.5f);
     }
 
     public void ButtonOptions() {
@@ -14,5 +17,9 @@ public class MenuScript : MonoBehaviour {
 
     public void ButtonExit() {
         Application.Quit(69);
+    }
+    public void Start_game()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
