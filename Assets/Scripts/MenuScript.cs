993f08510617b2e63e409908b5d7293764ea2ad2
Szymonq1 <126352791+Szymonq1@users.noreply.github.com>
2023-09-04 00:19:53 +0200

    CreateMenuTransition

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -9,6 +9,7 @@ public class MenuScript : MonoBehaviour {
 
     [SerializeField] private Animator mouse_animator;
     [SerializeField] private Animator clouds_animator;
+    [SerializeField] private Animator Transition_animator;
 
     public void Start() {
         Time.timeScale = 1.0f;
@@ -18,9 +19,14 @@ public class MenuScript : MonoBehaviour {
         menu_animator.Play("Main_menu");
         mouse_animator.Play("Mouse_run");
         clouds_animator.Play("Clouds_exit");
+        Invoke("MenuTransition", 1f); 
         Invoke(nameof(Start_game), 1.5f);
     }
 
+    private void MenuTransition()
+    {
+        Transition_animator.Play("MenuTransition");
+    }
     public void Start_game() {
         //SceneManager.UnloadSceneAsync("MainScene");
         SceneManager.LoadScene("SampleScene");
