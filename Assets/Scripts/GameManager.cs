a70bf716f0db1b0f498b5c20fe2a7c8995260d96
KumpelStachu <stanislaw.rzeszut@gmail.com>
2023-09-04 01:39:58 +0200

    AAAAAA

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 using System.Collections;
